974a3021edaad4c40eb9a6c9f252142938628d9b
kanta10250 <infrastructurekanta@gmail.com>
2025-02-13 03:19:47 +0900

    Update: app-function

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const game = () =>  {
   let pScore = 0;
   let cScore = 0;
-  const playBtn = document.querySelector('.intro button');
 
+  //Start the game
   const startGame = () => {
     const playBtn = document.querySelector('.intro button');
     const introScreen = document.querySelector('.intro');
@@ -10,11 +10,25 @@ const game = () =>  {
 
     playBtn.addEventListener('click', () => {
       introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
     });
   };
 
+  //play match
+  const playMatch = () => {
+    const options = document.querySelector('.options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('player-hand');
+    //computer options
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+      const computerNumber = Math.floor(Math.random() * 3);
+      console.log(computerNumber);
+  }
+
   //Is call all the inner function
   startGame();
+  playMatch();
 }
 
   //Start the game function
