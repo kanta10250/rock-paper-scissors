87d5d00fe06dfd6109506dfcc74c3c6b6ebc1378
kanta10250 <infrastructurekanta@gmail.com>
2025-02-13 01:40:10 +0900

    Update: animation

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const gama = () =>  {
+const game = () =>  {
   let pScore = 0;
   let cScore = 0;
   const playBtn = document.querySelector('.intro button');
-  
+
   const startGame = () => {
     const playBtn = document.querySelector('.intro button');
     const introScreen = document.querySelector('.intro');
-    const  match = document.querySelector('.match');
+    const match = document.querySelector('.match')
 
     playBtn.addEventListener('click', () => {
       introScreen.classList.add("fadeOut");
-    })
-  }
+    });
+  };
+
+  //Is call all the inner function
+  startGame();
 }
+
+  //Start the game function
+  game();
