07f7abadd08f14ea1c7daa15a376b1a8e2f33386
かんた <169161932+kanta10250@users.noreply.github.com>
2025-02-12 18:15:06 +0900

    Update: Js

---
+++ app.js
@@ -0,0 +1,15 @@
+const gama = () =>  {
+  let pScore = 0;
+  let cScore = 0;
+  const playBtn = document.querySelector('.intro button');
+  
+  const startGame = () => {
+    const playBtn = document.querySelector('.intro button');
+    const introScreen = document.querySelector('.intro');
+    const  match = document.querySelector('.match');
+
+    playBtn.addEventListener('click', () => {
+      introScreen.classList.add("fadeOut");
+    })
+  }
+}
